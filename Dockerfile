f76bd1983ac5d88c40ddeab4c435e3f9bde01328
Zax37 <c37zax@gmail.com>
2018-06-02 03:12:32 +0200

    Fixed build.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 ADD build/libs/transaction-generator.jar app.jar
-CMD java -Dserver.port=$PORT -jar app.jar
+CMD "java -Dspring.profiles.active=prod -Dserver.port=$PORT -jar /app.jar"
--- src/main/java/uj/jwzp/w2/e3/launchers/SpringMain.java
+++ src/main/java/uj/jwzp/w2/e3/launchers/SpringMain.java
@@ -1,20 +1,14 @@
 package uj.jwzp.w2.e3.launchers;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import uj.jwzp.w2.e3.logic.converter.YamlHttpMessageConverter;
 
 import java.util.List;
 
-@Slf4j
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "uj.jwzp.w2.e3")
+@SpringBootApplication
 public class SpringMain implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(SpringMain.class, args);
